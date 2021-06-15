f7ad8616cab841ee07ba043832453a696a7d59ae
egyapolley <steven02poli@yahoo.co.uk>
2021-06-15 20:24:35 +0000

    JsonStream package added

--- database/sql_database.js
+++ database/sql_database.js
@@ -9,6 +9,13 @@ const db_pass=process.env.DB_PASS
 const sequelize = new Sequelize(db_name,db_user, db_pass,{
     dialect:"mysql",
     host:"localhost",
+    logging:false,
+    pool: {
+        max: 20,
+        min: 0,
+        acquire: 10000,
+        idle: 10000
+    },
 
     define: {
         freezeTableName:true
